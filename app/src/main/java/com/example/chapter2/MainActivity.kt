14a7b3fab9a3e6f16a747988a7bd22cdf036d8f2
jiooong <lucy991010@gmail.com>
2024-05-19 03:11:45 +0900

    Feat : Constraint layout, view binding, onSaveInstanceState & onRestoreInstanceState 추가

--- app/src/main/java/com/example/chapter2/MainActivity.kt
+++ app/src/main/java/com/example/chapter2/MainActivity.kt
@@ -1,20 +1,75 @@
 package com.example.chapter2
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
+import com.example.chapter2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var inputNumber: Int = 0
+    var bool = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val view = binding.root
+        setContentView(view)
+
+        val inputTextView = binding.textview
+        val outputTextView = binding.textview3
+        val inputUnit = binding.centerInside
+        val outputUnit = binding.textview2
+        val snapButton = binding.snapButton
+
+
+        inputTextView.addTextChangedListener { text ->
+            inputNumber = if(text.isNullOrEmpty()){
+                0
+            } else{
+                text.toString().toInt()
+            }
+            if(bool){
+                outputTextView.text = inputNumber.times(0.01).toString()
+            }else{
+                outputTextView.text = inputNumber.times(100).toString()
+            }
         }
+
+        snapButton.setOnClickListener {
+            bool = !bool
+            if(bool){
+                inputUnit.text = "cm"
+                outputUnit.text = "m"
+                outputTextView.text = inputNumber.times(0.01).toString()
+            }else {
+                inputUnit.text = "m"
+                outputUnit.text = "cm"
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
+
+
+
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean("bool",bool)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle
+    ) {
+        bool = savedInstanceState.getBoolean("cmToM")
+        binding.textview2.text= if(bool) "cm" else "m"
+        binding.centerInside.text = if(bool) "m" else "cm"
+        super.onRestoreInstanceState(savedInstanceState)
     }
 }
